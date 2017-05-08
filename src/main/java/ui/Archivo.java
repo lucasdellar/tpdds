9c5d550783eb3f6e5699ee5cffdcbbcbf0f44b70
FischerMatias <mfischer00@gmail.com>
2017-05-07 21:33:29 -0300

    Agregado dialog para agregar archivo

---
+++ src/main/java/ui/Archivo.java
@@ -0,0 +1,16 @@
+package ui;
+
+/**
+ * Created by Matias Fischer on 07/05/2017.
+ */
+public class Archivo {
+    private String ruta;
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+}
---
+++ src/main/java/ui/ArchivoDialog.java
@@ -0,0 +1,7 @@
+package ui;
+
+/**
+ * Created by Matias Fischer on 07/05/2017.
+ */
+public class ArchivoDialog {
+}
