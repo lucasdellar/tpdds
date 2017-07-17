832f68f00480ccda0ea29508ff50241f7e92ddaf
lucas-avila <lucasrotelavila@gmail.com>
2017-07-17 18:40:37 -0300

    Merge

--- src/main/java/criterios/Sumatoria.java
+++ src/main/java/criterios/Sumatoria.java
@@ -1,10 +1,7 @@
 package criterios;
 
+
 import comparadores.IComparador;
-import condiciones.Condicion;
-import condiciones.CondicionTaxativa;
-import condiciones.CondicionPrioritaria;
-import domain.Indicador;
 import domain.Valor;
 import empresas.Empresa;
 
@@ -15,30 +12,20 @@ public class Sumatoria extends Criterio{
 		super(valor);
 	}
 
-	public double calcular(Empresa unaEmpresa, Condicion unaCondicion) {
-		return unaEmpresa.getCuentas().stream().mapToDouble(unaCuenta -> valor.calcular
-				(unaCuenta.getPeriodo(), unaEmpresa, unaCondicion.getRepoIndicadores())).sum();
+	@Override
+	public double calcular(Empresa unaEmpresa) {
+		return unaEmpresa.getCuentas().stream().mapToDouble(unaCuenta -> actualizarPeriodo(unaEmpresa, unaCuenta)).sum();
 	}
 	
 	@Override
-	public Boolean aplicar(Empresa unaEmpresa, double valor, IComparador comparador) {
+	public Boolean aplicar(Empresa unaEmpresa, double unValor, IComparador unComparador) {
 		
 		double sumatoria = 0;
 		
-		sumatoria = calcular(unaEmpresa, condicion_taxativa);
+		sumatoria = calcular(unaEmpresa);
 		
-		return condicion_taxativa.getComparador().comparar(sumatoria, condicion_taxativa.getValor());
-	}
-	
-	@Override
-	public Boolean aplicarPrioritaria(Empresa unaEmpresa, Empresa otraEmpresa, CondicionPrioritaria condicion_prioritaria) {
-		
-		double sumatoria1, sumatoria2 = 0;
-		
-		sumatoria1 = calcular(unaEmpresa, condicion_prioritaria);
-		sumatoria2 = calcular(otraEmpresa, condicion_prioritaria);
-		
-		return condicion_prioritaria.getComparador().comparar(sumatoria1, sumatoria2);
+		return unComparador.comparar(sumatoria, unValor);
 	}
 	
 }
+
