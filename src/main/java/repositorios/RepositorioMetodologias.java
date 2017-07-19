99d3f351e5a97280b7279018891c3d30c1533f15
SantiagoIlli <santiagoilli96@gmail.com>
2017-07-19 16:13:40 -0300

    Fix Travis

--- src/main/java/repositorios/RepositorioMetodologias.java
+++ src/main/java/repositorios/RepositorioMetodologias.java
@@ -3,5 +3,13 @@ package repositorios;
 import domain.Metodologia;
 
 public class RepositorioMetodologias extends Repositorio<Metodologia> {
+	
+	@Override
+	public void agregar(Metodologia unaMetodologia)
+	{
+		
+		
+	}
+	
 
 }
