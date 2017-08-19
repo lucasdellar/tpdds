26a1b13102061110a0f4a831b632e5ecee4070a5
lucas-avila <lucasrotelavila@gmail.com>
2017-08-19 00:59:43 -0300

    Detalle division por 0.

--- src/main/java/OperacionesMatematicas/Division.java
+++ src/main/java/OperacionesMatematicas/Division.java
@@ -1,8 +1,13 @@
 package OperacionesMatematicas;
 
+import domain.DomainExceptions.OperacionInvalidaException;
+
 public class Division implements Operador{
 
 	public double calcular(double operando1, double operando2) {
-		return operando1 / operando2; 
+		if(operando2 != 0)
+			return operando1 / operando2; 
+		else 
+			throw new OperacionInvalidaException("Se intento dividir por 0.");
 	}
 }
